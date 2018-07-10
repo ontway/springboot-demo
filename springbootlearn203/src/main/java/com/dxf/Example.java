a6260e5bde1f919e32c2658aaf5319dbd52289fd
duxiaofei <ontway@163.com>
2018-07-10 21:23:55 +0800

    spring boot learn 2.0.3

---
+++ springbootlearn203/src/main/java/com/dxf/Example.java
@@ -0,0 +1,21 @@
+package com.dxf;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+
+public class Example {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
